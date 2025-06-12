c53c23d23b19fb8ab6c26dbe443c0aaa44a66de6
David Luedtke <david@ldtke.de>
2025-06-12 11:32:14 +0200

    1.21.6

--- src/main/java/dev/kinau/betterpiechart/mixin/ProfilerPieChartMixin.java
+++ src/main/java/dev/kinau/betterpiechart/mixin/ProfilerPieChartMixin.java
@@ -16,12 +16,16 @@ public abstract class ProfilerPieChartMixin {
 
     @Shadow private String profilerTreePath;
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;III)I"))
-    private int renderPieChartText(GuiGraphics instance, Font font, String string, int i, int j, int k) {
-        if (!string.startsWith("["))
-            return instance.drawString(font, string, i, j, k);
-        if (string.startsWith("[0] root.") || string.equals("[?] unspecified"))
-            return instance.drawString(font, string, i, j, k);
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;III)V"))
+    private void renderPieChartText(GuiGraphics instance, Font font, String string, int i, int j, int k) {
+        if (!string.startsWith("[")) {
+            instance.drawString(font, string, i, j, k);
+            return;
+        }
+        if (string.startsWith("[0] root.") || string.equals("[?] unspecified")) {
+            instance.drawString(font, string, i, j, k);
+            return;
+        }
         int lastControlCharacter = profilerTreePath.lastIndexOf(30);
         String last = profilerTreePath.substring(Math.max(lastControlCharacter + 1, 0));
         String previousPath = profilerTreePath.substring(0, Math.max(lastControlCharacter, 0));
@@ -29,17 +33,25 @@ public abstract class ProfilerPieChartMixin {
         String previous = previousPath.substring(Math.max(previousControlCharacter + 1, 0));
         if (last.equals("entities")) {
             int firstBracketClose = string.indexOf(']');
-            if (firstBracketClose < 0) return instance.drawString(font, string, i, j, k);
+            if (firstBracketClose < 0) {
+                instance.drawString(font, string, i, j, k);
+                return;
+            }
             String entityType = string.substring(firstBracketClose + 2);
             if (Minecraft.getInstance().levelRenderer instanceof LevelRendererExpander levelRendererExpander) {
-                return instance.drawString(font, string + " (" + levelRendererExpander.betterPieChart$getLastVisibleEntityCounts().getOrDefault(entityType, 0L) + ")", i, j, k);
+                instance.drawString(font, string + " (" + levelRendererExpander.betterPieChart$getLastVisibleEntityCounts().getOrDefault(entityType, 0L) + ")", i, j, k);
+                return;
             }
         } else if (last.equals("blockentities") || previous.equals("blockentities")) {
             int firstBracketClose = string.indexOf(']');
-            if (firstBracketClose < 0) return instance.drawString(font, string, i, j, k);
+            if (firstBracketClose < 0) {
+                instance.drawString(font, string, i, j, k);
+                return;
+            }
             String blockEntityOrTag = string.substring(firstBracketClose + 2);
-            return instance.drawString(font, string + " (" + BlockEntityTracker.getInstance().counts().getOrDefault(blockEntityOrTag, 0L) + ")", i, j, k);
+            instance.drawString(font, string + " (" + BlockEntityTracker.getInstance().counts().getOrDefault(blockEntityOrTag, 0L) + ")", i, j, k);
+            return;
         }
-        return instance.drawString(font, string, i, j, k);
+        instance.drawString(font, string, i, j, k);
     }
 }
